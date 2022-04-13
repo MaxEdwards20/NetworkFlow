6647a2cbd3e4b8aac905a2ffb35b8df76113485f
medwards <a02276129@usu.edu>
2022-04-13 11:17:18 -0600

    Max flow is working correctly

--- src/Graph.java
+++ src/Graph.java
@@ -22,6 +22,8 @@ public class Graph {
 
         // This adds the actual requested edge, along with its capacity
         vertices[source].addEdge(source, destination, capacity);
+        vertices[destination].addEdge(destination, source, 0);
+
         residual[source][destination] = capacity;
         residual[destination][source]= 0;
         return true;
@@ -39,23 +41,19 @@ public class Graph {
             GraphNode v = vertices[t];
             while (v.parent != -1){
                 path.insert(0, v.id + " ");
-                int capacity = getCapacity(v.id);
-//                System.out.println("Capacity from " + v.parent + " to " + v.id + " is "+ capacity);
+                int capacity = residual[v.parent][v.id]; // This is the forward flow capacity
                 if (capacity < availableFlow){availableFlow = capacity;}
-                if (residual[v.parent][v.id] < availableFlow){
-                    availableFlow = residual[v.parent][v.id];
-                }
                 v = vertices[v.parent]; // Move the current up a level to its parent
             }
             v = vertices[t];
             while (v.parent != -1){
-                residual[v.parent][v.id] -= availableFlow;
-                residual[v.id][v.parent] += availableFlow;
+                residual[v.parent][v.id] -= availableFlow; // Forward flow
+                residual[v.id][v.parent] += availableFlow; // Backwards flow
                 v = vertices[v.parent];
             }
+            totalFlow += availableFlow;
             path.insert(0, "Flow " + availableFlow + ": "+ s + " ");
             totalPath.append(path).append("\n");
-            totalFlow += availableFlow;
         }
         if(report){
             System.out.println("-- Max Flow: " + name+ " --");
@@ -63,6 +61,32 @@ public class Graph {
             System.out.print(getEdgeReport());
         }
         return totalFlow;
+    }
+
+    /**
+     * Algorithm to find an augmenting path in a network
+     */
+    private boolean hasAugmentingPath(int s, int t) {
+        Queue<Integer> queue = new LinkedList<>();
+
+//        System.out.println("Augmenting paths values are s:" + s + " t:" + t);
+
+        // Reset all vertices to have no parent
+        for (var vertex: vertices) {vertex.parent = -1;}
+        queue.add(s);
+        while (!queue.isEmpty() && vertices[t].parent == -1){
+            int v = queue.remove();
+            for (var edge: vertices[v].successor){
+                int w = edge.to;
+                if (residual[v][w] > 0 && vertices[w].parent == -1 && w != s){
+                    vertices[w].parent = v;
+                    queue.add(w);
+                }
+            }
+        }
+        // Return false if the parent is non existent
+        // Return true if the parent has changed
+        return vertices[t].parent != -1;
     }
 
     private String getEdgeReport(){
@@ -78,49 +102,6 @@ public class Graph {
         }
         return sb.toString();
     }
-
-    /**
-     * Algorithm to find the capacity of an edge from a parent to a given node
-     * @param current node to
-     * @return integer value of the capacity to a node
-     */
-    private int getCapacity(int current){
-        GraphNode vertex = vertices[current];
-        GraphNode parent = vertices[vertex.parent];
-        for (GraphNode.EdgeInfo edge: parent.successor){
-            if (edge.to == current && edge.from == parent.id){
-                return edge.capacity;
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * Algorithm to find an augmenting path in a network
-     */
-    private boolean hasAugmentingPath(int s, int t) {
-        Queue<Integer> queue = new LinkedList<>();
-        // Reset all vertices to have no parent
-//        System.out.println("Augmenting paths values are s:" + s + " t:" + t);
-        for (var vertex: vertices) {
-            vertex.parent = -1;
-        }
-        queue.add(s);
-        while (!queue.isEmpty() && vertices[t].parent == -1){
-            int v = queue.remove();
-            for (var edge: vertices[v].successor){
-                int w = edge.to;
-                if (vertices[w].parent == -1 && residual[v][w] > 0 && w != s){
-                    vertices[w].parent = v;
-                    queue.add(w);
-                }
-            }
-        }
-        // Return false if the parent is non existent
-        // Return true if the parent has changed
-        return vertices[t].parent != -1;
-    }
-
 
     /**
      * Algorithm to find the min-cut edges in a network
